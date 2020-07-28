0c3a9838daffce91f90855e0de7774e57907d9b7
iamangelu <67723309+iamangelu@users.noreply.github.com>
2020-07-28 19:15:12 +0800

    Updates

---
+++ precache-manifest.3d39c832302473a8444994e2c781ae08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a06ea91d293d7da3a4f3e87cfe86ed",
+    "url": "/karakrus/index.html"
+  },
+  {
+    "revision": "728a7e9af067811739f6",
+    "url": "/karakrus/static/css/main.004d9708.chunk.css"
+  },
+  {
+    "revision": "789f77c16770f87c4797",
+    "url": "/karakrus/static/js/2.e9244381.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karakrus/static/js/2.e9244381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728a7e9af067811739f6",
+    "url": "/karakrus/static/js/main.22d45c05.chunk.js"
+  },
+  {
+    "revision": "232cc79e3d3f34324226",
+    "url": "/karakrus/static/js/runtime-main.0baf226c.js"
+  },
+  {
+    "revision": "87cfaa043b81e6e16b9af54567880b84",
+    "url": "/karakrus/static/media/head.87cfaa04.png"
+  },
+  {
+    "revision": "70f8c7f3b60713151f4a9d2b23b9a6c4",
+    "url": "/karakrus/static/media/tail.70f8c7f3.png"
+  }
+]);
